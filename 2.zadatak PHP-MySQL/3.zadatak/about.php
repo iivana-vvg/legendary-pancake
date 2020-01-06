b425862caf2f13a1a4c83667dc73d0207be5de93
iivana-vvg <56671856+iivana-vvg@users.noreply.github.com>
2020-01-06 19:44:10 +0100

    Add files via upload

---
+++ 2.zadatak PHP-MySQL/3.zadatak/about.php
@@ -0,0 +1,19 @@
+<?php 
+	print '
+	<h1>O nama</h1>
+	<h2>Vaš voditelj za putovanja</h2>
+	
+	<video width="50%" controls poster="img/rentals.jpg" >
+		  <source src="video/video.mp4" type="video/mp4">
+		  Vas preglednik ne podrzava HTML5 video.
+	</video>
+	
+	<p>Planiramo i organiziramo aktivnosti na terenu tijekom vašeg putovanja te kontaktirmo s kulturnim i drugim organizacijama.</p>
+		<p>Stručni turistički pojmovi su obično za putnika dodatna obaveza i nužno zlo koje s vremena na vrijeme mora savladati zbog samog putovanja. Jedan od pojmova koji se zadnjih godina vrti stalno u našim medijima je i destinacijski menadžment koji može biti koristan pojam kod privatnih putovanja diljem planete.</p>
+		<p>Destinacijski menadžment je u biti plansko upravljanje svim direktnim i indirektnim turističkim resursima pojedinačne destinacije u svrhu njenog dugoročnog razvoja. To, osim nositelja turističkih usluga, uključuje i sve kojima turizam nije primaran posao, ali bi se trebali brinuti oko njega ili biti nadopuna turističkoj ponudi poput sustava TZ, lokalne samouprave, institucija u kulturi, udruga...</p>
+		<p>Za provedbu destinacijskog turizma kroz konkretne ponude destinacije zadužena je agencija specijalizirana za destinacijski menadžment – Destination Management Company (DMC). Vas kao putnike dalje ne zanima gnjavaža agencija unutar složenog sustava razvoja destinacije i njegovo nefunkcioniranje već upravo agencije s oznakom DMC za pojedinu destinaciju kako je uspješna DMC agencija zapravo gospodar destinacije koja vam može ponuditi najbolje programe putovanja, turističke usluge i cijene. Za razliku od DMC-a, „obične“ agencije uglavnom same ne organiziraju složene sadržaje u destinaciji već uzimaju usluge upravo od DMC-a.</p>
+		
+		<p><b>Dakle, uputite nas što vas zanima i mi ćemo isplanirati privatna putovanja skrojena samo za vas i vaše potrebe.</b></p>
+		
+	<p>Pogledajte cijeli video na <a href="https://www.youtube.com/watch?v=uFcP6VJQ91Y" target="_blank">youtube.com</a></p>';
+?>
